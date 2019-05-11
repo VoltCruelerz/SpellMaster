da466b8967e26831341bfd574dd808378d0fe0b9
VoltCruelerz <mgreene11trigon@yahoo.com>
2019-05-11 00:09:24 -0400

    Add missing Hunter's Mark

--- SRD.js
+++ SRD.js
@@ -902,6 +902,23 @@ var SpellList = [
         Classes: "Bard, Paladin, SRD"
     },
     {
+        Name: "Hunter's Mark",
+        Level: 1,
+        School: "Divination",
+        IsRitual: false,
+        CastTime: "1 Bonus Action",
+        Range: "90 feet",
+        Components: {
+            V: true,
+            S: false,
+            M: false,
+            MDetails: ""
+        },
+        Duration: "Concentration, up to 1 hour",
+        Desc: "You choose a creature you can see within range and mystically mark it as your quarry.<br/>Until the spell ends, you deal an extra 1d6 damage to the target whenever you hit it with a weapon attack, and you have advantage on any Wisdom (Perception) or Wisdom (Survival) check you make to find it. If the target drops to 0 hit points before this spell ends, you can use a bonus action on a subsequent turn of yours to mark a new creature.<br/>At higher level: When you cast this spell using a spell slot of 3rd or 4th level, you can maintain your concentration on the spell for up to 8 hours.<br/>When you use a spell slot of 5th level or higher, you can maintain your concentration on the spell for up to 24 hours.",
+        Classes: "Ranger, SRD"
+    },
+    {
         Name: "Identify",
         Level: 1,
         School: "Divination",
